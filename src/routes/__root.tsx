ecb4280a69949d51a65f7ef2e76e8d401c3f8d85
davisampaiodev <davidsaps10@outlook.com>
2026-06-18 17:06:22 -0300

    add clarity

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -208,7 +208,19 @@ function RootShell({ children }: { children: ReactNode }) {
             `,
           }}
         />
-
+        <script
+  dangerouslySetInnerHTML={{
+    __html: `
+      (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;
+          t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];
+          y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "x94a65kanv");
+    `,
+  }}
+/>
         <noscript
           dangerouslySetInnerHTML={{
             __html: `
